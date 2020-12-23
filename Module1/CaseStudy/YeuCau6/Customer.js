628519948684d2416fe9c1966e2fd2d3eaac2d76
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2020-12-23 15:41:10 +0700

    update

---
+++ Module1/CaseStudy/YeuCau6/Customer.js
@@ -0,0 +1,94 @@
+class Customer {
+    constructor(name, CMND, birthday, email, address, typeCustomer, discount, quantinyIncluded, rendDays, typeService, typeRoom) {
+        this.name = name;
+        this.CMND = CMND;
+        this.birthday = birthday;
+        this.email = email;
+        this.address = address;
+        this.typeCustomer = typeCustomer;
+        this.discount = discount;
+        this.quantinyIncluded = quantinyIncluded;
+        this.rendDays = rendDays;
+        this.typeService = typeService;
+        this.typeRoom = typeRoom;
+    }
+    getName(){
+        return this.name;
+    }
+    getCMND(){
+        return this.CMND;
+    }
+    getBirthday(){
+        return this.birthday;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getAddress(){
+        return this.address;
+    }
+    getTypeCustomer(){
+        return this.typeCustomer;
+    }
+    getDiscount(){
+        return this.discount;
+    }
+    getQuantinyIncluded(){
+        return this.quantinyIncluded;
+    }
+    getRendDays(){
+        return this.rendDays;
+    }
+    getTypeService(){
+        return this.typeService;
+    }
+    getTypeRoom(){
+        return this.typeRoom;
+    }
+    setName(name){
+        this.name = name;
+    }
+    setCMND(CMND){
+        this.CMND = CMND;
+    }
+    setBirthday(birthday){
+        this.birthday = birthday;
+    }
+    setEmail(email){
+        this.email = email;
+    }
+    setAddress(address){
+        this.address = address;
+    }
+    setTypeCustomer(typeCustomer){
+        this.typeCustomer = typeCustomer;
+    }
+    setDiscount(discount){
+        this.discount = discount;
+    }
+    setQuantinyIncluded(quantinyIncluded){
+        this.quantinyIncluded = quantinyIncluded;
+    }
+    setRendDays(rendDays){
+        this.rendDays = rendDays;
+    }
+    setTypeService(typeService){
+        this.typeService = typeService;
+    }
+    setTypeRoom(typeRoom){
+        this.typeRoom = typeRoom;
+    }
+    totalPay(){
+        let money = 0;
+        if (this.getTypeService() === "Villa"){
+            money = 500;
+        }
+        else if (this.getTypeService() === "House"){
+            money = 300;
+        }
+        else  if (this.getTypeService() === "Room"){
+            money = 200;
+        }
+        return this.getRendDays() * money * (1 - this.getDiscount() / 100);
+    }
+}

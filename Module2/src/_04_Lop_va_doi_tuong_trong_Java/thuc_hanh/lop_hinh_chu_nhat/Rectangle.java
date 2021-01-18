0d87a90dd38c2d7e3d6339280bea5ef5bf664971
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2021-01-18 16:57:28 +0700

    Lop va doi tuong trong Java

--- Module2/src/_04_Lop_va_doi_tuong_trong_Java/thuc_hanh/lop_hinh_chu_nhat/Rectangle.java
+++ Module2/src/_04_Lop_va_doi_tuong_trong_Java/thuc_hanh/lop_hinh_chu_nhat/Rectangle.java
@@ -7,15 +7,12 @@ public class Rectangle {
         this.width = width;
         this.height = height;
     }
-
     public double getArea() {
         return this.width * this.height;
     }
-
     public double getPerimeter() {
         return (this.width + this.height) * 2;
     }
-
     public String display() {
         return "Rectangle{" + "width=" + width + ", height=" + height + "}";
     }

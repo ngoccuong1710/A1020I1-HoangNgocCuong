c3196361cc769f20d764b92a07f10ad5aa085995
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2021-01-27 18:09:43 +0700

    Ke thua

--- Module2/src/_06_Ke_thua/bai_tap/thiet_ke_va_trien_khai_lop_triangle/Triangle.java
+++ Module2/src/_06_Ke_thua/bai_tap/thiet_ke_va_trien_khai_lop_triangle/Triangle.java
@@ -1,4 +1,37 @@
 package _06_Ke_thua.bai_tap.thiet_ke_va_trien_khai_lop_triangle;
 
-public class Triangle {
+public class Triangle extends Shape{
+    private String color;
+
+    public Triangle() {
+    }
+
+    public Triangle(double side1, double side2, double side3, String color) {
+        super(side1, side2, side3);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea(){
+        return (Math.sqrt((getSide1() + getSide2() + getSide3()) * (getSide1() + getSide2() - getSide3())
+                * (getSide2() + getSide3() - getSide1()) * (getSide3() + getSide1() - getSide2()))) / 4;
+    }
+
+    public double getPerimeter(){
+        return getSide1() + getSide2() + getSide3();
+    }
+
+    @Override
+    public String toString() {
+        return "Tam giác có 3 cạnh: " + getSide1() + ", " + getSide2() + ", " + getSide3()
+                + ".\nMàu: " + getColor()
+                + ".\nCó diện tích là: " + getArea() + "\nChu vi là: " + getPerimeter();
+    }
 }

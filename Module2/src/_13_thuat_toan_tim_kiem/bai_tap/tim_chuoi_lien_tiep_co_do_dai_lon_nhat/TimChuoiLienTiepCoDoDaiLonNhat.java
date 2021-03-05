abe80ec289bc63e4d8f75967363e8f15a38ec4d1
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2021-03-05 19:38:34 +0700

    Thuat toan tim kiem

---
+++ Module2/src/_13_thuat_toan_tim_kiem/bai_tap/tim_chuoi_lien_tiep_co_do_dai_lon_nhat/TimChuoiLienTiepCoDoDaiLonNhat.java
@@ -0,0 +1,33 @@
+package _13_thuat_toan_tim_kiem.bai_tap.tim_chuoi_lien_tiep_co_do_dai_lon_nhat;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TimChuoiLienTiepCoDoDaiLonNhat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String string = input.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
+
+        // Find the maximum consecutive increasingly ordered substring
+        for (int i = 0; i < string.length(); i++) { 		// single loop
+            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+                list.clear();
+            }
+
+            list.add(string.charAt(i));
+
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+        }
+
+        for (Character ch: max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+}
---
+++ Module2/src/_13_thuat_toan_tim_kiem/bai_tap/tim_chuoi_tang_dan_co_do_dai_lon_nhat/TimChuoiTangDanCoDoDaiLonNhat.java
@@ -0,0 +1,35 @@
+package _13_thuat_toan_tim_kiem.bai_tap.tim_chuoi_tang_dan_co_do_dai_lon_nhat;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TimChuoiTangDanCoDoDaiLonNhat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String string = input.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+
+        // Find the maximum increasingly ordered subsequence
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                }
+            }
+
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+
+        for (Character ch: max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+}

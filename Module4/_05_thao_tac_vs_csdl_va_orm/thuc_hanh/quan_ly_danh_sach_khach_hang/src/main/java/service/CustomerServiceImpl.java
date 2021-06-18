1605187a022e5e607224cfef0305bf5cb4c45acd
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2021-06-18 21:24:42 +0700

    orm

--- Module4/_05_thao_tac_vs_csdl_va_orm/thuc_hanh/quan_ly_danh_sach_khach_hang/src/main/java/service/CustomerServiceImpl.java
+++ Module4/_05_thao_tac_vs_csdl_va_orm/thuc_hanh/quan_ly_danh_sach_khach_hang/src/main/java/service/CustomerServiceImpl.java
@@ -1,81 +1,35 @@
 package service;
 
 import model.Customer;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
-import javax.persistence.TypedQuery;
+import repository.CustomerRepository;
+import repository.CustomerRepositoryImpl;
 import java.util.*;
 
 public class CustomerServiceImpl implements CustomerService{
-
+    CustomerRepository customerRepository = new CustomerRepositoryImpl();
 
     @Override
-    public List findAll() {
-        String queryStr = "SELECT c FROM Customer AS c";
-        TypedQuery<Customer> query = ConnectionUtil.entityManager.createQuery(queryStr, Customer.class);
-        return query.getResultList();
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
     }
 
     @Override
     public void save(Customer customer) {
-        Session session = null;
-        Transaction transaction = null;
-
-        try{
-            session = ConnectionUtil.sessionFactory.openSession();
-            transaction = session.beginTransaction();
-
-            session.save(customer);
-            transaction.commit();
-        }catch (Exception e){
-            if (transaction != null){
-                transaction.rollback();
-            }
-        }finally {
-            if (session != null){
-                session.close();
-            }
-        }
+        customerRepository.save(customer);
     }
 
     @Override
     public Customer findById(int id) {
-        String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
-        TypedQuery<Customer> query = ConnectionUtil.entityManager.createQuery(queryStr, Customer.class);
-        query.setParameter("id", id);
-        return query.getSingleResult();
+        return customerRepository.findById(id);
     }
 
     @Override
     public Customer update(int id, Customer customer) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = ConnectionUtil.sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            Customer origin = findById(customer.getId());
-            origin.setName(customer.getName());
-            origin.setEmail(customer.getEmail());
-            origin.setAddress(customer.getAddress());
-            session.saveOrUpdate(origin);
-            transaction.commit();
-            return origin;
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (transaction != null) {
-                transaction.rollback();
-            }
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return null;
+        return customerRepository.update(id, customer);
     }
 
     @Override
     public void remove(int id) {
-
+        customerRepository.remove(id);
     }
 }

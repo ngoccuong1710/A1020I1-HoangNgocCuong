c75f624ae453b3be08302a6c5b63ef87f73e9962
leesyaoran123 <64198222+leesyaoran123@users.noreply.github.com>
2021-07-21 18:45:03 +0700

    session cookie

--- Module4/_10_session_cookie/bai_tap/gio_hang/src/main/java/vn/codegym/controller/CartController.java
+++ Module4/_10_session_cookie/bai_tap/gio_hang/src/main/java/vn/codegym/controller/CartController.java
@@ -1,4 +1,76 @@
 package vn.codegym.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import vn.codegym.model.Cart;
+import vn.codegym.model.Product;
+import vn.codegym.service.ProductService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/buy")
+    public String showCart(@SessionAttribute("/carts")HashMap<Long, Cart> cartHashMap, Model model){
+        model.addAttribute("carts", cartHashMap);
+        model.addAttribute("cartNum", cartHashMap.size());
+        model.addAttribute("cartMoney", totalPrice(cartHashMap));
+        return "cart";
+    }
+
+    @GetMapping("/buy/{id}")
+    public String addCart(@PathVariable Long id, @SessionAttribute("carts") HashMap<Long, Cart> cartHashMap, Model model){
+        if (cartHashMap == null){
+            cartHashMap = new HashMap<>();
+        }
+        Product product = productService.findById(id);
+        if (product != null){
+            if (cartHashMap.containsKey(id)){
+                Cart item = cartHashMap.get(id);
+                item.setQuantity(item.getQuantity() + 1);
+                cartHashMap.put(id, item);
+            }
+            else {
+                Cart item = new Cart();
+                item.setProduct(product);
+                item.setQuantity(1);
+                cartHashMap.put(id, item);
+            }
+        }
+        model.addAttribute("carts", cartHashMap);
+        model.addAttribute("cartNum", cartHashMap.size());
+        model.addAttribute("cartMoney", totalPrice(cartHashMap));
+        return "redirect:/cart/buy";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteCart(@PathVariable Long id, @SessionAttribute("carts") HashMap<Long, Cart> cartHashMap, Model model){
+        if (cartHashMap == null){
+            cartHashMap = new HashMap<>();
+        }
+        cartHashMap.remove(id);
+        model.addAttribute("carts", cartHashMap);
+        model.addAttribute("cartNum", cartHashMap.size());
+        model.addAttribute("cartMoney", totalPrice(cartHashMap));
+        return "redirect:/cart/buy";
+    }
+
+    public double totalPrice(HashMap<Long, Cart> cartHashMap){
+        int count = 0;
+        for (Map.Entry<Long, Cart> list:cartHashMap.entrySet()){
+            count += Double.parseDouble(list.getValue().getProduct().getNewPrice()) * list.getValue().getQuantity();
+        }
+        return count;
+    }
 }
--- Module4/_10_session_cookie/bai_tap/gio_hang/src/main/java/vn/codegym/controller/ProductController.java
+++ Module4/_10_session_cookie/bai_tap/gio_hang/src/main/java/vn/codegym/controller/ProductController.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import vn.codegym.model.Cart;
 import vn.codegym.model.Product;
 import vn.codegym.service.ProductService;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -32,6 +36,21 @@ public class ProductController {
     @PostMapping("/create")
     public String create(@ModelAttribute Product product){
         productService.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable Long id, Model model, @ModelAttribute("carts")HashMap<Long, Cart> cartHashMap){
+        Product product = productService.findById(id);
+        model.addAttribute("carts", cartHashMap);
+        model.addAttribute("product", product);
+        return "view";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        Product product = productService.findById(id);
+        productService.remove(product);
         return "redirect:/";
     }
 }

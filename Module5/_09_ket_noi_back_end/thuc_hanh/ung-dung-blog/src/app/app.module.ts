c5a369e50173a82309d4de38780eb8d10d38cee1
Cuong <64198222+leesyaoran123@users.noreply.github.com>
2021-09-15 00:33:54 +0700

    ket noi backend

--- Module5/_09_ket_noi_back_end/thuc_hanh/ung-dung-blog/src/app/app.module.ts
+++ Module5/_09_ket_noi_back_end/thuc_hanh/ung-dung-blog/src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
